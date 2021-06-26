8839e6dddd2245bdb6fb0f47f31ac19b4ce204ff
ingemarsc <ingemarsc@spgroup.com.sg>
2021-06-26 21:27:28 +0800

    add unit test

---
+++ database/voucher_test.go
@@ -0,0 +1,152 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+
+	"reflect"
+	"testing"
+	"time"
+
+	sqlmock "github.com/DATA-DOG/go-sqlmock"
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func setupSQLMock(t *testing.T) (*sql.DB, sqlmock.Sqlmock) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	return db, mock
+}
+
+func TestValidateVoucher(t *testing.T) {
+	db, mock := setupSQLMock(t)
+	defer db.Close()
+	fixtureEmail := "test@gmail.com"
+	fixtureCode := "code"
+	tests := []struct {
+		name       string
+		givenEmail string
+		givenCode  string
+		want       sql.NullTime
+		wantErr    bool
+	}{
+		{
+			name:       "used_at is null",
+			givenEmail: fixtureEmail,
+			givenCode:  fixtureCode,
+			want:       sql.NullTime{Valid: false},
+			wantErr:    false,
+		},
+		{
+			name:       "used_at is not null",
+			givenEmail: fixtureEmail,
+			givenCode:  fixtureCode,
+			want:       sql.NullTime{Valid: true, Time: time.Date(2021, time.June, 1, 0, 0, 0, 0, time.Local)},
+			wantErr:    false,
+		},
+		{
+			name:       "db error",
+			givenEmail: fixtureEmail,
+			givenCode:  fixtureCode,
+			want:       sql.NullTime{},
+			wantErr:    true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if !tt.wantErr {
+				mock.ExpectQuery("SELECT (.+) FROM customers").WithArgs(fixtureEmail, fixtureCode).WillReturnRows(sqlmock.NewRows([]string{"used_at"}).AddRow(tt.want))
+			} else {
+				mock.ExpectQuery("SELECT (.+) FROM customers").WithArgs(fixtureEmail, fixtureCode).WillReturnError(errors.New("error"))
+			}
+			got, err := ValidateVoucher(context.Background(), tt.givenEmail, tt.givenCode, sqlx.NewDb(db, "sqlmock"))
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ValidateVoucher() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ValidateVoucher() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSetVoucherUsageAndGetDiscount(t *testing.T) {
+	db, mock := setupSQLMock(t)
+	defer db.Close()
+	fixtureCode := "code"
+	tests := []struct {
+		name        string
+		givenCode   string
+		givenUsedAt sql.NullTime
+		want        float32
+		queryErr    bool
+		updateErr   bool
+	}{
+		{
+			name:        "get discount value",
+			givenCode:   fixtureCode,
+			givenUsedAt: sql.NullTime{Valid: true, Time: time.Now()},
+			want:        50.1,
+			queryErr:    false,
+			updateErr:   false,
+		},
+		{
+			name:        "query error",
+			givenCode:   fixtureCode,
+			givenUsedAt: sql.NullTime{Valid: true, Time: time.Now()},
+			want:        0,
+			queryErr:    true,
+			updateErr:   false,
+		},
+		{
+			name:        "update error",
+			givenCode:   fixtureCode,
+			givenUsedAt: sql.NullTime{Valid: true, Time: time.Now()},
+			want:        0,
+			queryErr:    false,
+			updateErr:   true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if !tt.queryErr {
+				mock.ExpectQuery("SELECT (.+) FROM special_offers").WithArgs(fixtureCode).WillReturnRows(sqlmock.NewRows([]string{"discount"}).AddRow(50.1))
+			} else {
+				mock.ExpectQuery("SELECT (.+) FROM special_offers").WithArgs(fixtureCode).WillReturnError(errors.New("error"))
+			}
+			mock.ExpectBegin()
+			mock.ExpectExec("UPDATE vouchers").WithArgs(tt.givenUsedAt.Time.Format(time.RFC3339), tt.givenCode).WillReturnResult(sqlmock.NewResult(1, 1))
+			if !tt.updateErr {
+				mock.ExpectCommit()
+			} else {
+				mock.ExpectRollback()
+			}
+
+			got, err := SetVoucherUsageAndGetDiscount(context.Background(), tt.givenCode, sqlx.NewDb(db, "sqlmock"))
+			if tt.queryErr {
+				if err == nil {
+					t.Errorf("SetVoucherUsageAndGetDiscount() error = %v, queryErr %v", err, tt.queryErr)
+					return
+				}
+			}
+			if tt.updateErr {
+				if err == nil {
+					t.Errorf("SetVoucherUsageAndGetDiscount() error = %v, updateErr %v", err, tt.updateErr)
+					return
+				}
+			}
+			if err == nil {
+				assert.EqualValues(t, tt.updateErr, false)
+				assert.EqualValues(t, tt.queryErr, false)
+			}
+			if got != tt.want {
+				t.Errorf("SetVoucherUsageAndGetDiscount() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/ingemar0720/voucher-pool
 go 1.14
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/go-chi/chi v1.5.4
 	github.com/jmoiron/sqlx v1.3.4
 	github.com/lib/pq v1.2.0
 	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.7.0
 )
